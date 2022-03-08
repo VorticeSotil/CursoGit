3e6a0e23fa6a67798ef395918f6085ffba6fcd7f
VorticeSotil <72065215+VorticeSotil@users.noreply.github.com>
2022-03-08 08:28:14 +0100

    Update funciones_sumar_restar.py

--- Funciones_operaciones/funciones_sumar_restar.py
+++ Funciones_operaciones/funciones_sumar_restar.py
@@ -1,5 +1,5 @@
 # PROGRAMA DE PRUEBA PARA PONER COMO EJEMPLO EN EL CURSO GIT
 
-# Función para sumar dos números.
+# Función para sumar dos números entre sí.
 def sumar_dos_numeros(num1, num2):
     return num1 + num2

a5f801f295f9e9a3585ac36a4321bd5e3b1e8da3
VorticeSotil <albertosotom@gmail.com>
2022-03-11 10:09:18 +0100

    Comentario en Listas Numericas

---
+++ Funciones_listas/funciones_listas_numericas.py
@@ -0,0 +1,6 @@
+'''
+Este script contendrá una función para crear una lista de números que el
+usuario introducirá por consola.
+El usuario decidirá qué y cuantos numeros introducir, y ademas decidirá
+cuando dejar de hacerlo.
+'''
